8cb32bf4d5f3acdef9ae15ca4330825085520156
xiuiworld <xiuiworld@gmail.com>
2026-05-08 00:12:17 +0900

    initialize springboot

---
+++ src/main/java/com/example/meanhwa_back/MeanhwaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.meanhwa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeanhwaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeanhwaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meanhwa_back/MeanhwaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meanhwa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeanhwaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
